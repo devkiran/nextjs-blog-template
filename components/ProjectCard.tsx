d7cb99f44c1917dcb463afd75af21bbacc3a6dc3
Kiran <kiran@boxyhq.com>
2022-07-16 21:13:29 +0530

    Add hyperlink

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,11 +1,22 @@
 import type { Project } from "types";
+import Link from "next/link";
 
 const ProjectCard = ({ project }: { project: Project; key: string }) => {
   return (
-    <div className="space-y-3 rounded-lg border-2 p-4">
-      <img src={project.image} alt={project.name} className="rounded-lg" />
-      <h2 className="text-xl font-bold text-gray-900">{project.name}</h2>
-      <p className="text-base">{project.description}</p>
+    <div className="rounded-lg border-2 p-4">
+      <Link href={project.url}>
+        <a>
+          <div className="space-y-3">
+            <img
+              src={project.image}
+              alt={project.name}
+              className="rounded-lg"
+            />
+            <h2 className="text-xl font-bold text-gray-900">{project.name}</h2>
+            <p className="text-base">{project.description}</p>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
--- data/projects.ts
+++ data/projects.ts
@@ -4,30 +4,35 @@ const projects = [
     description:
       "Supabase is an open source Firebase alternative. Start your project with a Postgres Database, Authentication, instant APIs, Realtime subscriptions and Storage.",
     image: "/projects/project.png",
+    url: "http://localhost:3000/",
   },
   {
     name: "SAML Jackson",
     description:
       "A SAML SSO service designed as an OAuth 2.0 flow. Integrate SAML with just a few lines of code.",
     image: "/projects/project.png",
+    url: "http://localhost:3000/",
   },
   {
     name: "Nhost",
     description:
       "Nhost is an open source Firebase alternative with GraphQL, built with the following things in mind: Open Source, GraphQL, SQL, Great Developer Experience.",
     image: "/projects/project.png",
+    url: "http://localhost:3000/",
   },
   {
     name: "Jitsu",
     description:
       "Jitsu is a fully-scriptable data ingestion engine for modern data teams. Set-up a real-time data pipeline in minutes, not days.",
     image: "/projects/project.png",
+    url: "http://localhost:3000/",
   },
   {
     name: "PostHog",
     description:
       "PostHog is the all-in-one platform for building better products - with product analytics, feature flags, session recordings, a/b testing, heatmaps.",
     image: "/projects/project.png",
+    url: "http://localhost:3000/",
   },
 ];
 
